9f37be82c341c577e970bd51d59e41019cb8599b
Desouky Reda <desoukyreda@std.mans.edu.eg>
2024-02-07 01:34:46 +0200

    change code of prob 3

--- w.05/3.c
+++ w.05/3.c
@@ -1,29 +1,17 @@
 # include<stdio.h>
-# define n 6
-int * average(int arr[n])
-{
-  int  sum[n/2]={0};float ave[n/2]={0};
-    float j=0;
-    int k = (int)j ;
-
-    for (size_t i = 0; i < 6;i++,j+=.5f)
-    {
-        k=j;
-
-        sum[k]+=arr[i];
-        ave[k]=sum[k]/2.0;
-    }
-   float * ptr = ave;
-    return ptr ;
-}
 int main()
 {
-    int arr[n] ={0,2,3,4,5,6};float *ptr;
-  ptr = average(arr);
-  printf("the average : ");
-    for (size_t i = 0; i < n/2; i++)
-    {
-        printf("%.02f,",ptr[i]);
-    }
-
+  int arr[5]={1,2,3,4,5},arr2[5]={0};
+  for (size_t i = 0; i < 5; i++)
+  {
+    arr2[i]=arr[5-i-1];
+  }
+  printf("the element after reverse : {");
+  for (size_t i = 0; i < 5; i++)
+  {
+    if(i==4)printf("%d}",arr2[i]);
+    else printf("%d,",arr2[i]);
+  }
+  
+  
 }
--- w.05/4.c
+++ w.05/4.c
@@ -1,24 +1,27 @@
 # include<stdio.h>
 # define n 6
-int * average(int arr[n])
+float ave[n/2];
+float * average(int arr[n])
 {
-  int  sum[n/2]={0};float ave[n/2]={0};
+  int  sum[n/2]={0};
     float j=0;
     int k = (int)j ;
     
-    for (size_t i = 0; i < 6;i++,j+=.5f)
+    for (size_t i = 0; i < 5;i++,j+=.5f)
     {
         k=j;
         
         sum[k]+=arr[i]; 
-        ave[k]=sum[k]/2.0; 
+        if(j != k)
+        ave[k]=sum[k]/2.0;
+        else ave[k]=sum[k]; 
     }
-   float * ptr = ave;
+  static float * ptr = ave;
     return ptr ;
 }
 int main()
 {
-    int arr[n] ={0,2,3,4,5,6};float *ptr;
+    int arr[n] ={0,2,3,4,5};float *ptr;
   ptr = average(arr);
   printf("the average : ");
     for (size_t i = 0; i < n/2; i++)
